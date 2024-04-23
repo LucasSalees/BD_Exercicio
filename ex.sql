376df1ed7670e9e7468a458de82893758da1dd8b
LucasSalees <saleslucaas01@gmail.com>
2024-04-22 21:46:44 -0300

    Update ex.sql

--- ex.sql
+++ ex.sql
@@ -9,11 +9,13 @@ CREATE TABLE Fornecedor
     CNPJ VARCHAR (15),
     endereco VARCHAR (40)
 );
+go
 CREATE TABLE Produto (
     Codigo INT PRIMARY KEY,
     Nome VARCHAR(30),
     Descricao VARCHAR(30)
 );
+go
 CREATE TABLE Venda (
     Num_NFfiscal INT PRIMARY KEY,
     Data DATE,
@@ -21,11 +23,13 @@ CREATE TABLE Venda (
     Codigo_Produto INT,
     Codigo_Cliente INT
 );
+go
 CREATE TABLE Loja (
     Codigo INT PRIMARY KEY,
     Nome VARCHAR(30),
     CNPJ VARCHAR(15)
 );
+go
 CREATE TABLE Cliente (
     Codigo INT PRIMARY KEY,
     Nome VARCHAR(30),
